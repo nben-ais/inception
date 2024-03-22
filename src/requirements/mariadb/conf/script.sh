c2c14349830801fea4e7db5b3441a3b0cc65ca58
nben-ais <nouhailabenaissa466@gmail.com>
2024-03-22 13:44:15 +0000

    to haitham

--- src/requirements/mariadb/conf/script.sh
+++ src/requirements/mariadb/conf/script.sh
@@ -3,3 +3,4 @@ service mysql start
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
